c8138ef31c5419b5caf43c2d8144a216af4b810e
alston789 <alstontham@gmail.com>
2020-08-26 23:02:10 +0800

    implemented Level-1 additions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String greeting = "___________________________________________________________\n"
-                + "Hello! I'm Duke\n"
-                + "What can I do for you?\n"
-                + "___________________________________________________________\n";
-        String farewell = "Bye. Hope to see you again soon!\n"
-                + "___________________________________________________________\n";
-        System.out.println(greeting + farewell);
+        String userInput;
+        System.out.println("Hey there! I'm Duke\n" + "What would you like to do?");
+
+        //this do-while loop asks users for input and echoes until it receives a specific string
+        do {
+            Scanner in = new Scanner(System.in);
+            userInput = in.nextLine();
+            echoString(userInput);
+        } while (!userInput.equals("bye"));
+
+        System.out.println("Bye, see you soon!");
+    }
+
+    //this function outputs the same string as the user inputs
+    public static void echoString(String userInput) {
+        System.out.println(userInput + "\n");
     }
 }
+
+
