1c786526c28746ae147b03b9a8fd5456bd74d952
alston789 <alstontham@gmail.com>
2020-08-27 08:19:44 +0800

    added storage and listing of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,22 +3,34 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String userInput;
-        System.out.println("Hey there! I'm Duke\n" + "What would you like to do?");
+        String[] listOfItems = new String[100];
+        int listItemNum = 0;
+        System.out.println("Hey there! I'm Duke\n" + "What would you like to do?" + "\n");
 
-        //this do-while loop asks users for input and echoes until it receives a specific string
+        //this do-while loop asks users for input, can add items to lists and print the list
         do {
             Scanner in = new Scanner(System.in);
             userInput = in.nextLine();
-            echoString(userInput);
-        } while (!userInput.equals("bye"));
+            if (userInput.equals("list")) {
+                //goes through each item in the array and lists them out
+                for (int i = 0; i < listItemNum; i++) {
+                    System.out.println((i + 1) + ". " + listOfItems[i]);
+                }
+                System.out.print("\n");
+            } else if (userInput.equals("bye")) {
+                //exits the do-while loop and exits the program
+                break;
+            } else {
+                //prints out items that users want to add to a list then adds it to the list
+                System.out.println("added: " + userInput + "\n");
+                listOfItems[listItemNum] = userInput;
+                listItemNum++;
+            }
+        } while (true);
 
         System.out.println("Bye, see you soon!");
     }
 
-    //this function outputs the same string as the user inputs
-    public static void echoString(String userInput) {
-        System.out.println(userInput + "\n");
-    }
 }
 
 
